ab2716ce79e83262bd1181e7e8dea32e8587c9be
René Wollny <wollny.rene@gmail.com>
2023-07-14 22:50:16 +0200

    started implementing navbar and frameworks

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const board = document.getElementById("board");
+const cells = [];
+
+// function createBoard() {
+//     for (let i = 0; i < 9; i++) {
+//         const cell = document.createElement("div");
+//         cell.classList.add("cell");
+//         cell.classList.add("image");
+//         board.appendChild(cell);
+//         cells.push(cell);
+//         cell.addEventListener("click", function () {
+//             makeMove(i)
+//         });
+//     };
+// };
